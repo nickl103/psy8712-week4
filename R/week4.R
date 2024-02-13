9a5075b7ba5770b4161e953c59d348d98adc7a41
nickl103 <nickl103@umn.edu>
2024-02-12 22:48:22 -0600

    convert datadate, replace zeros w/ NA, and dropped unusable cases

--- R/week4.R
+++ R/week4.R
@@ -7,3 +7,6 @@ import_tbl <- read_delim("../data/week4.dat", delim= "-", col_names = c("casenum
 glimpse(import_tbl)
 wide_tbl <- separate_wider_delim(import_tbl, cols=qs, delim="-", names= c("q1", "q2", "q3", "q4", "q5"))
 wide_tbl[5:9] <- sapply(wide_tbl[5:9], as.integer)
+wide_tbl$datadate <- mdy_hms(wide_tbl$datadate)
+wide_tbl[5:9] <- replace(wide_tbl[5:9], wide_tbl[5:9] == 0, values = NA)
+wide_tbl <- drop_na(wide_tbl, q2)
